4ac57ab65025e0f426f4afa9d76ad1b9a29362b0
mezzeddinee <mazenezzeddine7@gmail.com>
2026-05-16 15:46:37 +0200

    Implement error handling in GreenHello initialization
    Added error handling during initialization of GreenHello class.

--- GreenPilott/GreenPilottCommands.py
+++ GreenPilott/GreenPilottCommands.py
@@ -1,12 +1,20 @@
 from pilotTools import CommandBase
+import traceback
 
 
 class GreenHello(CommandBase):
 
     def __init__(self, pilotParams):
 
-        self.pp = pilotParams
-        self.log = pilotParams.log
+        try:
+            CommandBase.__init__(self, pilotParams, "GreenHello")
+            print("GREENHELLO INIT OK")
+
+        except Exception as e:
+            print("GREENHELLO INIT FAILED")
+            print(str(e))
+            traceback.print_exc()
+            raise
 
     def execute(self):
 
