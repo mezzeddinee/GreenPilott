941be1953a13a5edf7dffb631c08f1f97cb1e1d5
mezzeddinee <mazenezzeddine7@gmail.com>
2026-05-23 15:36:44 +0200

    Refactor GreenHello class initialization and execution
    Removed exception handling and debug prints from GreenHello class.

--- GreenPilott/GreenPilottCommands.py
+++ GreenPilott/GreenPilottCommands.py
@@ -1,25 +1,12 @@
 from pilotTools import CommandBase
-import traceback
 
+print("GREENPILOTT MODULE IMPORTED v2026-05-23-1")
 
 class GreenHello(CommandBase):
-
     def __init__(self, pilotParams):
-
-        try:
-            CommandBase.__init__(self, pilotParams, "GreenHello")
-            print("GREENHELLO INIT OK")
-
-        except Exception as e:
-            print("GREENHELLO INIT FAILED")
-            print(str(e))
-            traceback.print_exc()
-            raise
+        CommandBase.__init__(self, pilotParams, "GreenHello")
+        print("GREENHELLO INIT OK v2026-05-23-1")
 
     def execute(self):
-
-        print("################################")
-        print("### GREEN HELLO FROM MAZEN ###")
-        print("################################")
-
+        print("GREENHELLO EXECUTE v2026-05-23-1")
         return 0
